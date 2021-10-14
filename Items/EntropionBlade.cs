a8b00518a486ff6466d35ce91ee9c0aacac01b6e
DevaVade <86522749+DevaVade@users.noreply.github.com>
2021-10-14 11:58:27 +0800

    Create EntropionBlade.cs

---
+++ Items/EntropionBlade.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VadesContentMod.Items.Weapons
+{
+	public class EntropionBlade : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Entropion Blade");
+			Tooltip.SetDefault("amogus(insert text epic text here if u wany)");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 55000;
+			item.melee = true;
+			item.width = 150;
+			item.height = 200;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 11;
+			item.UseSound = SoundID.Item1; 
+			item.autoReuse = true;
+		}
+
+		public override void ModifyTooltips(List<TooltipLine> list)
+		{
+			foreach (TooltipLine tooltipLine in list)
+			{
+				if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+				{
+					tooltipLine.overrideColor = new Color?(new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB));
+				}
+			}
+		}
+	}
+}
