480614110fbb4217d9f2b30b40da92b36b9b863c
DevaVade <86522749+DevaVade@users.noreply.github.com>
2021-10-15 17:02:30 +0800

    fixed a typo

--- Items/suspotion.cs
+++ Items/suspotion.cs
@@ -5,7 +5,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Localization;
 
-namespace VadesContenMod.Items
+namespace VadesContentMod.Items
 {
     public class suspotion : ModItem
     {
