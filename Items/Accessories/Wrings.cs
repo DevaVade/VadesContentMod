a70a13591c0cd5fb19367cf3ab5e1058b52e7229
ElCholoGamer <josedanielgrayson@gmail.com>
2021-10-15 08:47:09 -0500

    add wrings

---
+++ Items/Accessories/Wrings.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VadesContentMod.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Wings)]
+    public class Wrings : ModItem
+    {
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("This is a modded wing.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 22;
+			item.height = 20;
+			item.value = Item.sellPrice(gold: 5);
+			item.rare = ItemRarityID.Purple;
+			item.accessory = true;
+		}
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.wingTimeMax = 180;
+		}
+
+		public override void VerticalWingSpeeds(Player player, ref float ascentWhenFalling, ref float ascentWhenRising,
+			ref float maxCanAscendMultiplier, ref float maxAscentMultiplier, ref float constantAscend)
+		{
+			ascentWhenFalling = 0.85f;
+			ascentWhenRising = 0.15f;
+			maxCanAscendMultiplier = 1f;
+			maxAscentMultiplier = 3f;
+			constantAscend = 0.135f;
+		}
+
+		public override void HorizontalWingSpeeds(Player player, ref float speed, ref float acceleration)
+		{
+			speed = 9f;
+			acceleration *= 2.5f;
+		}
+	}
+}
