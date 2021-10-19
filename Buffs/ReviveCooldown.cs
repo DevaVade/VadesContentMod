f0d5bb7a2277ae32dc5f135d27ce9bb8cd329212
ElCholoGamer <josedanielgrayson@gmail.com>
2021-10-19 11:27:04 -0500

    add destructor revive (needs testing)

---
+++ Buffs/ReviveCooldown.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VadesContentMod.Buffs
+{
+    public class ReviveCooldown : ModBuff
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            texture = "Terraria/Buff_" + BuffID.Confused;
+            return base.Autoload(ref name, ref texture);
+        }
+
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Revive Cooldown");
+            Description.SetDefault("You can't revive");
+
+            Main.debuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetModPlayer<VadPlayer>().reviveCooldown = true;
+        }
+    }
+}
