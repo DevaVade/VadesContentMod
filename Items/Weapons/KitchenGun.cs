5191a158c492ace9d267c8fd4e1cfe103f19e9fe
ElCholoGamer <josedanielgrayson@gmail.com>
2021-11-02 19:50:55 -0500

    add kitchen gun

---
+++ Items/Weapons/KitchenGun.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VadesContentMod.Items.Weapons
+{
+    public class KitchenGun : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.ranged = true;
+            item.noMelee = true;
+            item.damage = 50;
+            item.knockBack = 2f;
+            item.useAnimation = 12;
+            item.useTime = 4;
+            item.reuseDelay = 14;
+            item.shoot = 10;
+            item.shootSpeed = 10f;
+            item.useAmmo = AmmoID.Bullet;
+            item.UseSound = SoundID.Item31;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.value = Item.sellPrice(gold: 5);
+            item.rare = ItemRarityID.Pink;
+            item.autoReuse = true;
+        }
+
+        public override bool ConsumeAmmo(Player player) => player.itemAnimation >= item.useAnimation - 2;
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Vector2 velocity = new Vector2(speedX, speedY);
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 46f;
+
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+            }
+
+            if (player.itemAnimation < 4)
+            {
+                type = ModContent.ProjectileType<Projectiles.KitchenBullet>();
+                knockBack *= 2f;
+            } else
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Projectile.NewProjectile(position, velocity.RotatedByRandom(MathHelper.ToRadians(3)), type, damage / 2, knockBack, player.whoAmI);
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public override Vector2? HoldoutOffset() => new Vector2(6f, 2f);
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+
+            recipe.AddIngredient(ItemID.ClockworkAssaultRifle);
+            recipe.AddIngredient(ItemID.SoulofMight, 20);
+            recipe.AddIngredient(ItemID.HallowedBar, 15);
+
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/KitchenBullet.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VadesContentMod.Projectiles
+{
+    public class KitchenBullet : ModProjectile
+    {
+        private float spriteScale = 0f;
+
+        public override void SetDefaults()
+        {
+            projectile.width = projectile.height = 12;
+            projectile.friendly = true;
+            projectile.ranged = true;
+            projectile.extraUpdates = 3;
+        }
+
+        public override void AI()
+        {
+            if (projectile.ai[0] == 0f)
+            {
+                projectile.ai[0] = 1f;
+
+                projectile.velocity *= 0.5f;
+
+                float maxSpeed = 10f;
+                float speed = projectile.velocity.Length();
+                if (speed > maxSpeed)
+                    projectile.velocity *= maxSpeed / speed;
+            }
+
+            
+            {
+                Dust dust = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, DustID.LifeDrain, Scale: 1.5f);
+
+                Vector2 direction = Vector2.Normalize(projectile.velocity).RotatedBy(MathHelper.PiOver2);
+                if (Main.rand.NextBool())
+                    direction *= -1f;
+
+                dust.velocity = direction * dust.velocity.Length() * 1.6f;
+                dust.noGravity = true;
+            }
+
+            if (Main.rand.NextBool(12)){
+                Dust dust = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, DustID.TopazBolt, Scale: 1f);
+                dust.velocity = Vector2.Zero;
+            }
+
+            spriteScale = MathHelper.Min(spriteScale + 0.3f, 1f);
+            projectile.rotation = projectile.velocity.ToRotation();
+
+            Lighting.AddLight(projectile.Center, Color.Red.ToVector3() * 0.6f);
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.immune[projectile.owner] = 2;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Main.PlaySound(SoundID.Item14, projectile.Center);
+
+            int size = 16;
+            Rectangle hitbox = new Rectangle((int)projectile.Center.X - size / 2, (int)projectile.Center.Y - size / 2, size, size);
+
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                NPC npc = Main.npc[i];
+                if (npc.active && !npc.dontTakeDamage && npc.Hitbox.Intersects(hitbox))
+                {
+                    int hitDirection = Math.Sign(npc.Center.X - projectile.Center.X);
+                    npc.StrikeNPC(projectile.damage / 2, projectile.knockBack / 2, hitDirection);
+                }
+            }
+
+            for (int d = 0; d < 8; d++)
+            {
+                Dust dust = Dust.NewDustDirect(hitbox.TopLeft(), hitbox.Width, hitbox.Height, DustID.LifeDrain, Scale: 2f);
+                dust.noGravity = true;
+                dust.velocity *= 2f;
+            }
+
+            for (int d = 0; d < 4; d++)
+            {
+                Dust dust = Dust.NewDustDirect(hitbox.TopLeft(), hitbox.Width, hitbox.Height, DustID.TopazBolt, Scale: 1.2f);
+                dust.velocity *= 1.5f;
+            }
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D texture = Main.projectileTexture[projectile.type];
+
+            Color color = projectile.GetAlpha(lightColor);
+            Vector2 origin = new Vector2(texture.Width - 12, texture.Height / 2);
+            SpriteEffects spriteEffects = projectile.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipVertically;
+
+            spriteBatch.Draw(
+                texture,
+                projectile.Center - Main.screenPosition,
+                null,
+                color,
+                projectile.rotation,
+                origin,
+                projectile.scale * spriteScale,
+                spriteEffects,
+                0f);
+
+            return false;
+        }
+    }
+}
