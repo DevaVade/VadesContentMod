acf0fbb61209936d81fd320642a0f9d51db82afe
ElCholoGamer <josedanielgrayson@gmail.com>
2021-12-16 12:35:58 -0500

    added dev team

---
+++ Items/Placeable/DevTeam.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VadesContentMod.Items.Placeable
+{
+    class PeakEvolution : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Dev Team");
+            Tooltip.SetDefault("Deva Vade#4493");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 46;
+            item.height = 32;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.rare = ItemRarityID.Purple;
+            item.value = Item.buyPrice(0, 20, 0, 0);
+            item.createTile = ModContent.TileType<Tiles.DevTeam>();
+        }
+    }
+}
---
+++ Tiles/DevTeam.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace VadesContentMod.Tiles
+{
+    public class DevTeam : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.Origin = new Point16(4, 3);
+            TileObjectData.newTile.Width = 7;
+            TileObjectData.newTile.Height = 7;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16, 16, 16, 16 };
+
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.addTile(Type);
+
+            disableSmartCursor = true;
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Painting");
+            AddMapEntry(new Color(99, 50, 30), name);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 48, 48, ModContent.ItemType<Items.Placeable.DevTeam>());
+        }
+    }
+}
